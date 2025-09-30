74a9741b19b6bc60de03865585328ac3036b2a5c
liz-acosta <elizabeth.r.acosta@gmail.com>
2025-09-29 20:48:47 -0700

    good stopping point

--- app.py
+++ app.py
@@ -1,10 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import pokemon
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+
+    attributes_dict = {}
+    
+    for key, value in pokemon.POKEMON_ATTRIBUTES.items():
+
+        pokemon_attributes_list = pokemon.get_attributes(value)
+        attributes_dict[key] = pokemon_attributes_list
+
+    return render_template("index.html", pokemon_attributes=pokemon.POKEMON_ATTRIBUTES, attributes_dict=attributes_dict)
+
+@app.route("/pokemon", methods = ['POST', 'GET'])
+def display_pokemon():
+
+    form_data = request.form.to_dict()
+    print(form_data)
+    
+    pokemon_attribute = list(request.form.keys())[0]
+    pokemon_selection = form_data[pokemon_attribute]
+    print(pokemon_attribute)
+
+    pokemon_list = pokemon.get_list_by_attribute(pokemon_attribute, pokemon_selection)
+    if request.method == 'POST':
+        print(request.form.keys())
+    return render_template("pokemon.html", pokemon_list=pokemon_list)
+   
+#    else:
+#       user = request.args.get('name')
+
+#     pokemon.get_list_by_attribute
+#     return render_template("pokemon.html", pokemon=pokemon)
+
+# @app.route('/login',methods = ['POST', 'GET'])
+# def login():
+#    if request.method == 'POST':
+#       user = request.form['name']
+#       return redirect(url_for('dashboard',name = user))
+#    else:
+#       user = request.args.get('name')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- pokemon.py
+++ pokemon.py
@@ -7,49 +7,58 @@ POKEMON_ATTRIBUTES = {'Ability': 'ability',
 
 BASE_URL = 'https://pokeapi.co/api/v2/'
 
-class Pokemon:
+def get_attributes(attribute):
+
+    response = requests.get(BASE_URL + attribute)
+
+    response_json = response.json()
+
+    attributes_list = [item['name'] for item in response_json['results']]
+
+    return attributes_list
     
-    def get_list_by_attribute(attribute_endpoint, attribute):
-        
-        response = requests.get(BASE_URL + attribute_endpoint + '/' + attribute)
-
-        pokemon_list = [item['pokemon']['name'] for item in response.json()['pokemon']]
-
-        return pokemon_list
+def get_list_by_attribute(attribute, selection):
     
-    def get_pokemon_by_name(name):
+    response = requests.get(BASE_URL + POKEMON_ATTRIBUTES[attribute] + '/' + selection)
 
-        response = requests.get(BASE_URL + 'pokemon/' + name)
+    pokemon_list = [item['pokemon']['name'] for item in response.json()['pokemon']]
 
-        response_json = response.json()
+    return pokemon_list
 
-        pokemon = {'id': response_json['id'],
-                   'name': response_json['name'],
-                   'types': [item['type']['name'] for item in response_json['types']],
-                   'abilities': [item['ability']['name'] for item in response_json['abilities']],
-                   'image_url': response_json['sprites']['front_default']}
-        
-        return pokemon
+def get_pokemon_by_name(name):
+
+    response = requests.get(BASE_URL + 'pokemon/' + name)
+
+    response_json = response.json()
+
+    pokemon = {'id': response_json['id'],
+                'name': response_json['name'],
+                'types': [item['type']['name'] for item in response_json['types']],
+                'abilities': [item['ability']['name'] for item in response_json['abilities']],
+                'image_url': response_json['sprites']['front_default']}
     
-    def get_pokemon_characteristics_by_id(pokemon):
+    return pokemon
 
-        response = requests.get(BASE_URL + 'pokemon-species/' + pokemon['name'])
+def get_pokemon_characteristics_by_id(pokemon):
 
-        response_json = response.json()
+    response = requests.get(BASE_URL + 'pokemon-species/' + pokemon['name'])
 
-        description = next(filter(lambda x: x['version']['name'] == 'firered', response_json['flavor_text_entries']), None)
+    response_json = response.json()
 
-        pokemon.update({'description': description['flavor_text'].replace('\n', ' '),
-                        'color': response_json['color']['name']}) 
+    description = next(filter(lambda x: x['version']['name'] == 'firered', response_json['flavor_text_entries']), None)
 
-        return pokemon
+    pokemon.update({'description': description['flavor_text'].replace('\n', ' '),
+                    'color': response_json['color']['name']}) 
 
+    return pokemon
 
-test = Pokemon.get_list_by_attribute('type', 'ground')
+print(get_attributes('type'))
 
-test_pokemon = Pokemon.get_pokemon_by_name('eevee')
+# test = get_list_by_attribute('type', 'ground')
 
-test_test_pokemon = Pokemon.get_pokemon_characteristics_by_id(test_pokemon)
+test_pokemon = get_pokemon_by_name('eevee')
+
+test_test_pokemon = get_pokemon_characteristics_by_id(test_pokemon)
 
 print(test_pokemon)
 
